f1f3d2b72be4feec72e70053fe73ef6e706989f9
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-03-21 17:45:31 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,13 +1,27 @@
 import sys
 
-def Usuario(entrada,x = 1 ):
-	if (65<=(ord(usuario_1[n]))<=90 or 97<= (ord(usuario_1[n]))<= 122):
-		n +=1
-		x +=1 
-		Verificador(usuario_1,x)
+def Usuario(entrada,x = 1):
+	if  (65 >=entrada[x]<= 90 or 97 >=entrada[x]<= 122 ):
+		x +=1
+		Usuario(entrada)
 
-	elif (65<=(ord(usuario_1[n]))<=90 or 97<= (ord(usuario_1[n]))<= 122):
-		n +=1
-		x +=1 
-		Verificador(usuario_1,x)
+	elif (48 >=entrada[x]<= 57):
+		x +=1
+		Exp_Num(entrada,x)
 
+	else:
+		print("Usuario no valido")
+		Usuario(input("\nIngrese su nombre de usuario:"))
+
+
+def Exp_Num(entrada,x):
+	entrada_largo =len(entrada)
+
+	if (48 >=entrada[x]<= 57):
+		Exp_Num(entrada)
+
+	if entrada_largo == x:
+		if(entrada[x]>) 
+
+
+Usuario(input("Ingrese su nombre de usuario: "))
