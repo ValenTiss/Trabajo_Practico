2e27b0cf19f3479e245a6b10770980f8174b7491
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-03-22 10:01:42 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,31 +1,59 @@
+"""
+Autores:
+Profesor:
+Fecha de entrega
+"""
 import sys
 
-def Usuario(entrada,x = 1):
-	if  (65 <=entrada[x]<= 90 or 97 <=entrada[x]<= 122 ):
-		x +=1
-		Usuario(entrada)
+def Usuario(entrada,x):
+	entrada_len = len(entrada)
+	if(x < entrada_len):
+		if ( 90>= ord(entrada[x]) >= 65 or 122>= ord(entrada[x]) >= 97):
+			x +=1
+			Usuario(entrada,x)
 
-	elif (48 <=entrada[x]<= 57):
-		x +=1
-		Exp_Num(entrada,x)
+		elif (48 <= (ord(entrada[x])) <= 57):
 
-<<<<<<< HEAD
+			Exp_Num(entrada,x,0)
+
+		else:
+			print("No entro")
+
 	else:
-		print("Usuario no valido")
-		Usuario(input("\nIngrese su nombre de usuario:"))
+		print("Usuario no valido.")
+		Usuario(input("Ingrese su usuario: "), 0 )
 
 
-def Exp_Num(entrada,x):
-	entrada_largo =len(entrada)
+def Exp_Num(Num,x,n):
+	entrada_largo =len(Num)
+	ult_entrada = entrada_largo-4
+	if (n < 2):
+		if (48 <= (ord(Num[x])) <= 57):
+			x +=1
+			n +=1
+			entrada_largo -=1
+			Exp_Num(Num,x,n)
 
-	if (48 <=entrada[x]<= 57):
-		Exp_Num(entrada)
+		else:
+			print("Usuario no valido.")
+			Usuario(input("\nIngrese su nombre de usuario:"),0)
 
-	if entrada_largo == x:
-		if(entrada[x]>) 
+	if n == 2:
+		if (33<= (ord(Num[x])) >=38 or (ord(Num[x])) == 63 ): 
+			print("Entrada exitosa.")
+			n +=1
+			Menu
 
+		else:
+			print("Usuario no valido.")
+			Usuario(input("\nIngrese su nombre de usuario:"),0)
 
-Usuario(input("Ingrese su nombre de usuario: "))
-=======
+	elif n > 2:
+		print("Usuario no valido.")
+		Usuario(input("\nIngrese su nombre de usuario:"),0)
 
->>>>>>> ec59ce1c291cc3809e6dac02c58ce6abb031e03c
+def Menu():
+	print("Bienvenido.")
+
+	
+Usuario(input("Ingrese su usuario: "), 0 )
