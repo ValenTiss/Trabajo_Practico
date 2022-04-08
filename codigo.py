f54bda56fc5381797457d3a58b61ab7a7ca8b198
FedeAlfaro <71413273+FedeAlfaro@users.noreply.github.com>
2022-04-08 15:35:05 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -252,3 +252,164 @@ def Aceptar_nombre_cajero(posible_usuario,exp_regular,contador):  #Verifica que 
 
 Solicitar_cuenta()
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
+Función que se encarga de verificar que el monto en la cuenta del usuario tenga el monto que desea retirar y llama a otras funciones para encargarse del proceso de retirar dinero de la cuenta del usuario y descontar los billetes de los cajeros
+"""
+def retirar_dinero(monto, dinero_del_usuario, dinero_en_cajero):
+  """
+  Entradas: Monto (número natural), dinero_del_usuario (int), dinero_cajero (string)
+  Los valores deben estar verificados cuando entran como parámetros
+  """
+  dinero_en_cajero_int= convertir_string_a_dinero(dinero_en_cajero) # dinero total del cajero en int
+  if monto <= dinero_del_usuario: #verifica si el usuario tiene el monto en su cuenta
+    if monto  <= dinero_en_cajero_int: #verifica si el cajero tiene el monto ingresado disponible para retirar
+      retirar_dinero_del_cajero(monto, dinero_en_cajero) #Se llama a la función que elige la menor cantidad de billetes para devolver 
+      #------------actualizar historial -------  volver al menú principal
+    else: 
+      print("Dinero no disponible en este cajero, por favor desplácese a otro cajero o vuelva en unos días cuando se llenen las reservas")
+  else:
+    print("Fondos insuficientes para realizar esta transacción. Para darle el mejor servicio el Banco #### pone a su dispocisión el servicio CRÉDITO EN LA PALMA DE SU MANO, para saber más acerca de este servicio pregunte en la sucursal más cercana")
+
+#Convierte los string que contienen la cantidad de billetes de cada denominación a un entero donde cada denominación está en el fomato "1,2,5,10,20,50,100" donde en este caso cada número representa el valor del billete, sin embargo, en cada casilla se escribe la cantida de billetes
+def convertir_string_a_dinero(string):
+  lista_billetes= string.split(",") #Lista que contiene la cantidad de billetes por denominación
+  return int(lista_billetes[0]) + int(lista_billetes[1])*2 + int(lista_billetes[2])*5 + int(lista_billetes[3])*10 + int(lista_billetes[4])*20 + int(lista_billetes[5])*50 + int(lista_billetes[6])*100
+
+#Se encarga de devolver la menor cantidad de billetes, actualiza la cantidad de billetes en el cajero  y dinero disponible del usuario
+def retirar_dinero_del_cajero(monto, dinero_en_cajero):
+  billete_100= devolver_billete_100(monto, dinero_en_cajero)
+  monto -= billete_100*100
+  billete_50= devolver_billete_50(monto, dinero_en_cajero)
+  monto -= billete_50*50
+  billete_20= devolver_billete_20(monto, dinero_en_cajero)
+  monto -= billete_20*20
+  billete_10= devolver_billete_10(monto, dinero_en_cajero)
+  monto -= billete_10*10
+  billete_5= devolver_billete_5(monto, dinero_en_cajero)
+  monto -= billete_5*5
+  billete_2= devolver_billete_2(monto, dinero_en_cajero)
+  monto -= billete_2*2
+  billete_1= devolver_billete_1(monto, dinero_en_cajero)
+  monto -= billete_1*1
+  if monto==0:
+    #------ actualizar la cantidad de billetes en el cajero  y dinero disponible del usuario
+    print(monto)
+    print(str(billete_1)+","+str(billete_2)+","+str(billete_5)+","+str(billete_10)+","+str(billete_20)+","+str(billete_50)+","+str(billete_100)) #cantidad de billetes de cada denominación que se están retirando 
+    return print("Transacción realizada con éxito, reciba su dinero")
+  else:
+    print("No es posible realizar el retiro en este cajero debido a la falta de billetes necesarios")
+
+#calcula la cantidad de billetes de 100 que se deben y pueden devolver
+def devolver_billete_100(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_100_cajero=  int(dinero_en_cajero.split(",")[6]) #cantidad de billetes de 100 en el cajero
+  if monto>100: #si el dinero a retirar es mayor a 100
+    billetes_necesarios= monto // 100  # Posible cantidad de billetes de 100 a retirar
+    if billetes_necesarios <= billetes_100_cajero: # si la cantidad de posibles billetes 100 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_100_cajero
+  else:
+    return 0
+
+#calcula la cantidad de billetes de 50 que se deben y pueden devolver
+def devolver_billete_50(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_50_cajero=  int(dinero_en_cajero.split(",")[5]) #cantidad de billetes de 50 en el cajero
+  if monto>50: #si el dinero a retirar es mayor a 50
+    billetes_necesarios= monto // 50  # Posible cantidad de billetes de 50 a retirar
+    if billetes_necesarios <= billetes_50_cajero: # si la cantidad de posibles billetes 50 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_50_cajero
+  else:
+    return 0
+
+#calcula la cantidad de billetes de 20 que se deben y pueden devolver
+def devolver_billete_20(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_20_cajero=  int(dinero_en_cajero.split(",")[4]) #cantidad de billetes de 20 en el cajero
+  if monto>20: #si el dinero a retirar es mayor a 20
+    billetes_necesarios= monto // 20  # Posible cantidad de billetes de 20 a retirar
+    if billetes_necesarios <= billetes_20_cajero: # si la cantidad de posibles billetes 20 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_20_cajero
+  else:
+    return 0
+
+#calcula la cantidad de billetes de 10 que se deben y pueden devolver
+def devolver_billete_10(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_10_cajero=  int(dinero_en_cajero.split(",")[3]) #cantidad de billetes de 10 en el cajero
+  if monto>10: #si el dinero a retirar es mayor a 10
+    billetes_necesarios= monto // 10  # Posible cantidad de billetes de 10 a retirar
+    if billetes_necesarios <= billetes_10_cajero: # si la cantidad de posibles billetes 10 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_10_cajero
+  else:
+    return 0
+
+#calcula la cantidad de billetes de 5 que se deben y pueden devolver
+def devolver_billete_5(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_5_cajero=  int(dinero_en_cajero.split(",")[2]) #cantidad de billetes de 5 en el cajero
+  if monto>5: #si el dinero a retirar es mayor a 5
+    billetes_necesarios= monto // 5  # Posible cantidad de billetes de 5 a retirar
+    if billetes_necesarios <= billetes_5_cajero: # si la cantidad de posibles billetes 5 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_5_cajero
+  else:
+    return 0
+
+
+#calcula la cantidad de billetes de 2 que se deben y pueden devolver
+def devolver_billete_2(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_2_cajero=  int(dinero_en_cajero.split(",")[1]) #cantidad de billetes de 2 en el cajero
+  if monto>2: #si el dinero a retirar es mayor a 2
+    billetes_necesarios= monto // 2  # Posible cantidad de billetes de 2 a retirar
+    if billetes_necesarios <= billetes_2_cajero: # si la cantidad de posibles billetes 2 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_2_cajero
+  else:
+    return 0
+
+
+#calcula la cantidad de billetes de 1 que se deben y pueden devolver
+def devolver_billete_1(monto, dinero_en_cajero):
+  """
+  Entradas: Monto (int), dinero_en_cajero (string)
+  """
+  billetes_1_cajero=  int(dinero_en_cajero.split(",")[0]) #cantidad de billetes de 1 en el cajero
+  if monto>1: #si el dinero a retirar es mayor a 1
+    billetes_necesarios= monto // 1  # Posible cantidad de billetes de 1 a retirar
+    if billetes_necesarios <= billetes_1_cajero: # si la cantidad de posibles billetes 1 a retirar están en el cajero
+      return billetes_necesarios
+    else:
+      return billetes_1_cajero
+  else:
+    return 0
+
+
+
+monto= 4
+dinero_del_usuario= 3
+dinero_en_cajero= "1,1,1,1,1,1,1"
+retirar_dinero(monto, dinero_del_usuario, dinero_en_cajero)
