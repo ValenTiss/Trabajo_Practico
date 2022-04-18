135d5e981a107a80dbc70e6ff5f2902d93c8be66
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-04-18 17:09:21 -0600

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,29 +2,33 @@ diccionario_historial = {}
 diccionario_clave = {}
 diccionario_saldo = {}
 diccionario_cajero = {}
+clave = "usuarios.txt"
+historial = "historial.txt"
+cajero = "cajero.txt"
+saldo = "saldo.txt"
 def CrearCuenta(opcion):
 	if opcion == "Usuario":
-		cuentasArchivo = open("usuarios.txt")
+		cuentasArchivo = open(clave)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_us = Splitter(cuenta,0,diccionario_clave) 
 		diccionario_u = EntValue(diccionario_us)
 		return diccionario_u
 
 	elif opcion == "Saldo":
-		cuentasArchivo = open("saldo.txt")
+		cuentasArchivo = open(saldo)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_us = Splitter(cuenta,0,diccionario_saldo)
 		diccionario_u = EntValue(diccionario_us)
 		return diccionario_u
 
 	elif opcion == "Cajero":
-		cuentasArchivo = open("cajero.txt")
+		cuentasArchivo = open(cajero)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_ca = Splitter(cuenta,0,diccionario_cajero)
 		return diccionario_ca
 
 	elif opcion == "Historial":
-		cuentasArchivo = open("historial.txt")
+		cuentasArchivo = open(historial)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_h = Splitter(cuenta,0,diccionario_historial)
 		return diccionario_h
@@ -46,7 +50,9 @@ def Splitter(cuenta,x,diccionario):
 			keyRec = key
 			valueRec = value
 			diccionario[keyRec] = valueRec
+			diccionario[keyRec]= valueRec.replace("\\n","\n")
 			x+=1
+
 			Splitter(cuenta,x,diccionario)
 	else:
 		return diccionario
@@ -57,9 +63,35 @@ def EntValue(diccionario):
 	diccionario = dict((k, int(v)) for k, v in diccionario.items())
 	return diccionario
 
+def Actualizar(filepath,diccionario):
+	string = str(diccionario)
+	if filepath == historial:
+		string = string.replace("'","")
+		string = string.replace(":  ",":")
+		len_string = len(string)
+		abrirDocumento = open(filepath,"w")
+		abrirDocumento.write(string[1:len_string-1])
+		abrirDocumento.close()
+
+
+	else:
+		string = string.replace("'","")
+		string = string.replace(":","")
+		len_string = len(string)
+		abrirDocumento = open(filepath,"w")
+		abrirDocumento.write(string[1:len_string-1])
+		abrirDocumento.close()
+
+def Finalizar():
+	Act_clave =Actualizar(clave,diccionario_clave)
+	Act_saldo = Actualizar(saldo,diccionario_saldo)
+	Act_cajero = Actualizar(cajero,diccionario_cajero)
+	Act_historial = Actualizar(historial,diccionario_historial)
+	
 
 diccionario_clave = CrearCuenta("Usuario")
 diccionario_saldo = CrearCuenta("Saldo")
 diccionario_cajero = CrearCuenta("Cajero")
 diccionario_historial = CrearCuenta("Historial")
-print(diccionario_cajero)
+
+Finalizar()
