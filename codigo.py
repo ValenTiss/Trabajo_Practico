c9cffa7ff6f8453a6b1f9fde9abe9dee9f8291c5
ValenTiss <100385088+ValenTiss@users.noreply.github.com>
2022-04-18 17:08:56 -0600

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -5,15 +5,17 @@ Fecha de entrega: 19 / 04 /22
 """
 import sys
 import time
-diccionario = {}
 diccionario_historial = {}
 diccionario_clave = {}
 diccionario_saldo = {}
 diccionario_cajero = {}
+clave = "usuarios.txt"
+historial = "historial.txt"
+cajero = "cajero.txt"
+saldo = "saldo.txt"
 
 #La funcion Usuario se utiliza para determinar si los caracteres son letras en minuscula o mayuscula.
 def Usuario(entrada,x): #??? no deberíamos cambiar el parámetro x por algo más significatico?
-
 	entrada_len = len(entrada)
 	#Se crea una variable para hacer que la funcion sea recursiva.
 	if(x < entrada_len):
@@ -614,27 +616,27 @@ def Num_Cajero(Cajero,NmbCajero,x):
 
 def CrearCuenta(opcion):
 	if opcion == "Usuario":
-		cuentasArchivo = open("usuarios.txt")
+		cuentasArchivo = open(clave)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_us = Splitter(cuenta,0,diccionario_clave) 
 		diccionario_u = EntValue(diccionario_us)
 		return diccionario_u
 
 	elif opcion == "Saldo":
-		cuentasArchivo = open("saldo.txt")
+		cuentasArchivo = open(saldo)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_us = Splitter(cuenta,0,diccionario_saldo)
 		diccionario_u = EntValue(diccionario_us)
 		return diccionario_u
 
 	elif opcion == "Cajero":
-		cuentasArchivo = open("cajero.txt")
+		cuentasArchivo = open(cajero)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_ca = Splitter(cuenta,0,diccionario_cajero)
 		return diccionario_ca
 
 	elif opcion == "Historial":
-		cuentasArchivo = open("historial.txt")
+		cuentasArchivo = open(historial)
 		cuenta = cuentasArchivo.readlines()
 		diccionario_h = Splitter(cuenta,0,diccionario_historial)
 		return diccionario_h
@@ -656,7 +658,9 @@ def Splitter(cuenta,x,diccionario):
 			keyRec = key
 			valueRec = value
 			diccionario[keyRec] = valueRec
+			diccionario[keyRec]= valueRec.replace("\\n","\n")
 			x+=1
+
 			Splitter(cuenta,x,diccionario)
 	else:
 		return diccionario
@@ -667,6 +671,31 @@ def EntValue(diccionario):
 	diccionario = dict((k, int(v)) for k, v in diccionario.items())
 	return diccionario
 
+def Actualizar(filepath,diccionario):
+	string = str(diccionario)
+	if filepath == historial:
+		string = string.replace("'","")
+		string = string.replace(":  ",":")
+		len_string = len(string)
+		abrirDocumento = open(filepath,"w")
+		abrirDocumento.write(string[1:len_string-1])
+		abrirDocumento.close()
+
+
+	else:
+		string = string.replace("'","")
+		string = string.replace(":","")
+		len_string = len(string)
+		abrirDocumento = open(filepath,"w")
+		abrirDocumento.write(string[1:len_string-1])
+		abrirDocumento.close()
+
+def Finalizar():
+	Act_clave =Actualizar(clave,diccionario_clave)
+	Act_saldo = Actualizar(saldo,diccionario_saldo)
+	Act_cajero = Actualizar(cajero,diccionario_cajero)
+	Act_historial = Actualizar(historial,diccionario_historial)
+	
 
 diccionario_clave = CrearCuenta("Usuario")
 diccionario_saldo = CrearCuenta("Saldo")
@@ -675,3 +704,4 @@ diccionario_historial = CrearCuenta("Historial")
 
 
 Solicitar_cuenta()
+Finalizar()
